50305d901dbe93fae86c434aa1dd0dec8f33af9d
Raviel Chausse <ravielchausse@outlook.com>
2017-12-18 10:15:11 -0200

    init create filters

--- src/balance/List.vue
+++ src/balance/List.vue
@@ -1,8 +1,24 @@
 <template lang="html">
-    <div id="balance">
-        
-        <button id="show-modal" class="btn btn-primary" @click="showModal = true">Nova Compra</button>
-        
+    <div id="balance" class="container">
+        <form name="form-filters" class="form-inline">
+            <div class="row">
+                <div class="form-group">
+                    <label for="select-purchasers">Comprador: </label>
+                    <select id="select-purchasers" name="select-purchasers" class="form-control">
+                        <option value="0" selected></option>
+                        <option v-for="purchaser in purchaserLst" :value="purchaser.pur_id">{{ purchaser.pur_name }}</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="btn-search"></label>
+                    <button id="btn-search" class="btn btn-success" @click.prevent.default="onSearch($event)">Pesquisar</button>
+                </div>
+                <div class="form-group">
+                    <label for="show-modal"></label>
+                    <button id="show-modal" class="btn btn-primary" @click.prevent.default="showModal = true">Nova Compra</button>
+                </div>
+            </div>
+        </form>
         <balance-form 
             :showModal="showModal" 
             :purchaserLst="purchaserLst" 
@@ -10,7 +26,6 @@
             :paymentLst="paymentLst" 
             :skillLst="skillLst" 
             @close="showModal = false" />
-
     </div>
 </template>
 
@@ -40,6 +55,9 @@ export default {
             this.priorityLst = data.priorityLst
             this.paymentLst = data.paymentLst
             this.skillLst = data.skillLst
+        },
+        onSearch (evt) {
+            console.log({ evt })
         }
     }
 }
